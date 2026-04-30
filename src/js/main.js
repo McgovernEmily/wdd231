b33696cdb0fbb0ca89098dbf34605999b24c1703
McGovernEmily <101mcgoverne@gmail.com>
2026-04-30 16:46:08 -0600

    changing stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,21 @@
 import { getParkData, parkInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setheaderfooter.mjs";
-import setHeaderInfo from "./setheaderfooter.mjs";
-import {mediaCardTemplate} from "./templates.mjs";
-
+import { mediaCardTemplate } from "./templates.mjs";
 const parkData = getParkData();
 
 
-function sectionIntro(data){
+function setParkIntro(data) {
   const introEl = document.querySelector(".intro");
-  introEl.innerHTML = `
-  <h1>${parkData.fullName}</h1>
+  introEl.innerHTML = `<h1>${parkData.fullName}</h1>
   <p>${parkData.description}</p>`;
 }
 
-
-function settingLinks(data){
+function setParkInfoLinks(data) {
   const infoEl = document.querySelector(".info");
   const html = data.map(mediaCardTemplate);
   infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
 setHeaderFooter(parkData);
-sectionIntro(parkData);
-settingLinks(parkInfoLinks);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -9,31 +9,28 @@ export function parkInfoTemplate(info) {
 export function mediaCardTemplate(info) {
   return `<div class="media-card">
     <a href="${info.link}">
-    <img src="${info.image}" alt="${info.name}" class="media-card_img">
+    <img src="${info.image}" alt="${info.name}" class="media-card__img">
     <h3 class="media-card_title">${info.name}</h3>
     </a>
    <p>${info.description}</p>
      </div>`;
 }
-
-function getMailAddress(addresses){
+function getMailingAddress(addresses) {
   const mailing = addresses.find((address) => address.type === "Mailing");
   return mailing;
 }
-
-function getVoiceNumber(numbers){
+function getVoicePhone(numbers) {
   const voice = numbers.find((number) => number.type === "Voice");
   return voice.phoneNumber;
 }
-
 export function footerTemplate(info) {
-  const mailing = getMailAddress(info.addresses);
-  const voice = getVoiceNumber(info.contacts.phoneNumbers);
+  const mailing = getMailingAddress(info.addresses);
+  const voice = getVoicePhone(info.contacts.phoneNumbers);
 
   return `<section class="contact">
     <h3>Contact Info</h3>
     <h4>Mailing Address:</h4>
-    <div><p>${mailing.line1}</p>
+    <div><p>${mailing.line1}<p>
     <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p></div>
     <h4>Phone:</h4>
     <p>${voice}</p>
